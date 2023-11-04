ea51a3cabe117b63c2906f8009cba355d91c367e
auqid <auqidirfan100@gmail.com>
2023-11-04 18:21:13 +0530

    Two components Header and home added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Home from './modules/Home/Home';
 
 export default function App() {
   return (
-    <h1 className="text-gray-600 text-4xl">
-      Hello world!
+    <h1 classNameName="text-gray-600 text-4xl">
+      <Header></Header>
+      <Home/>
     </h1>
   )
 }
