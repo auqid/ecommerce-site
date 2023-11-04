673a73fe87c0356a74a724653165effbdd97a708
auqid <auqidirfan100@gmail.com>
2023-11-04 21:19:53 +0530

    product, feature, footer added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Home from './modules/Home/Home';
+import Footer from './components/Footer/Footer';
+
 
 export default function App() {
   return (
     <h1 classNameName="text-gray-600 text-4xl">
       <Header></Header>
       <Home/>
+      <Footer/>
     </h1>
   )
 }
