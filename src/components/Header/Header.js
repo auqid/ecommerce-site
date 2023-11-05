70d1d5e81c586c83f83ee44916026549735fad05
auqid <auqidirfan100@gmail.com>
2023-11-05 20:28:02 +0530

    added route to cart button TODO: ADD redux store to store cart items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,11 +40,13 @@ const Header = () => {
         )
       })}
     </nav>
+    <Link to={'/cart'}>
     <button className="inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0">Cart
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
     </button>
+    </Link>
     <button className="inline-flex items-center bg-gray-100 border-0 py-2 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
--- src/components/Hero/Home.js
+++ src/components/Hero/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import shopping from '../../assets/shopping.png'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
@@ -10,10 +11,11 @@ const Hero = () => {
       <h1 className="font-sans md:font-serif title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 ">Budget friendly Deals.
         <br className="hidden lg:inline-block"/>
       </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 leading-relaxed">World's Leading E-commerce business!</p>
       <div className="flex justify-center">
+        <Link to={'/products'}>
         <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Shop Now!</button>
-        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+        </Link>
       </div>
     </div>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 shadow-lg">
