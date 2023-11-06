4721074d8828e72ab7d28de26e790fc788f76109
auqid <38567390+auqid@users.noreply.github.com>
2023-11-07 05:26:47 +0530

    changes to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -47,11 +47,11 @@ const Header = () => {
       </svg>
     </button>
     </Link>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-2 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+    {/* <button className="inline-flex items-center bg-gray-100 border-0 py-2 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
-    </button>
+    </button> */}
   </div>
 </header>
     </div>
