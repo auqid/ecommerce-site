b9405cd1cd7e0a7cfe9f4e4e54c55a4e1db3b909
auqid <auqidirfan100@gmail.com>
2023-11-09 22:39:24 +0530

    space

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -60,7 +60,7 @@ const Header = () => {
       })}
     </nav>
     {isAuthenticated&& <Link to={'/cart'}>
-    <button className="inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0">Cart ({getTotal().totalQuantity})Items
+    <button className="inline-flex items-center text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0">Cart ({getTotal().totalQuantity}) Items
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
